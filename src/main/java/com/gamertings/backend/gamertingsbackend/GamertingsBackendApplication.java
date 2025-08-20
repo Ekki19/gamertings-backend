3aacdd3b8bbf362d5d80ba0dd012516a17241cd7
Jan Ekgardt <jan.ekgardt@gmail.com>
2025-08-20 17:36:37 +0200

    initial commit

---
+++ src/main/java/com/gamertings/backend/gamertingsbackend/GamertingsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gamertings.backend.gamertingsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamertingsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamertingsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gamertings/backend/gamertingsbackend/GamertingsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamertings.backend.gamertingsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamertingsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
